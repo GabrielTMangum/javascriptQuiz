e94d34dc3fab3a6cafd477f2c33e47b2f8f99e81
GabrielTMangum <gabrieltmangum@gmail.com>
2019-10-24 00:38:47 -0400

    continude work on building my functions but I still can't get the quesiton validator to fuction correctly

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,4 +1,5 @@
 var startBtn = document.getElementById("start-button")
+
 var startScrn = document.getElementById("start-screen")
 var qaScrn = document.getElementById("qa-screen")
 var questionHeading = document.getElementById("question");
@@ -7,58 +8,44 @@ var timerDisplay = document.getElementById("timer-display")
 var whereAreWe = 0;
 var timeLeft = 75
 
-console.log(questions[whereAreWe].answer)
+
+
 
 startBtn.addEventListener("click", function(event) {
     startScrn.classList.add("d-none");
     qaScrn.classList.remove("d-none");
-
+    
     startTimer();
-
+    
     questionHeading.textContent = questions[whereAreWe].title;
     answerList.innerHTML = "";
-
+    
     var answerOptions = questions[whereAreWe].choices;
     for(var i = 0 ; i< answerOptions.length; i++) {
         var newChoice = document.createElement("button");
         var buttonSpacing = document.createElement("div");
-
+        
         newChoice.classList.add("btn", "btn-primary", "float-left")
         newChoice.textContent = questions[whereAreWe].choices[i];
-        newChoice.setAttribute("value", questions[whereAreWe].choices[i])
         newChoice.setAttribute("id", i)
         buttonSpacing.classList.add("row", "text-center", "p-3")
         answerList.appendChild(newChoice);
         buttonSpacing.appendChild(newChoice);
         answerList.appendChild(buttonSpacing)
     }
+    var option1 = document.getElementById("0").textContent;
+    var option2 = document.getElementById("1").textContent;
+    var option3 = document.getElementById("2").textContent;
+    var option4 = document.getElementById("3").textContent;
 
+    console.log(option1 + option2 + option3 + option4)
+    
+    
+    answerList.addEventListener("click", function answerValidator(event){
+        event.stopPropagation();
+        
+    })
 })
-if (document.getElementById("0").value === questions[whereAreWe].answer){
-    alert("boo")
-} else if (document.getElementById("1").value === questions[whereAreWe].answer){
-    alert("boo")
-} else if (document.getElementById("2").value === questions[whereAreWe].answer){
-    alert("boo")
-} else if (document.getElementById("3").value === questions[whereAreWe].answer){
-    alert("boo")
-}
-
-function answerValidator(event){
-    event.stopPropagation();
-    alert("boo")
-//     if(newChoice.textContent === questions[whereAreWe].answer)
-//     {
-//         alert("boo")
-//         nextQuestion();
-//     } else {
-//         timeLeft -= 15;
-//     }
-}
-
-function nextQuestion () {
-
-}
 
 function startTimer () {
 
